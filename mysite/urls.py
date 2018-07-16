c33ee60f0a5ff71f5c2093101a39548fd0032404
chenxuanling <xuanlingchen12@gmail.com>
2018-07-16 23:08:05 +0800

    added view and template

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path
+import blog.urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+#     url(r'', include('blog.urls')),
+    url(r'', include(blog.urls)),
+#     url(r'^$', views.post_list, name='post_list'),
 ]
